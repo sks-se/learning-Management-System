18e021b296206b56b1d7d355975f2b409ef199fa
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-06-20 02:37:14 +0530

    Add Instructor class extending User

---
+++ Instructor.java
@@ -0,0 +1,6 @@
+public class Instructor extends User{
+public  Instructor(String instructorName, String instructorEmail, String instructorPassword)
+    {
+        super(name, email, password);
+    }
+}
