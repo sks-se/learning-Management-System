e6ebe9b9d2c0b08479ceb80def2ed0a68a73af86
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-06-21 04:40:24 +0530

    Implement Student class with profile and dashboard methods
    used the profile and dashboard method from the User class.
    used polymorphism override type

--- Student.java
+++ Student.java
@@ -1,6 +1,34 @@
+import java.lang.Override;
+import java.util.Scanner;
+
 public class Student extends User {
 
     public Student(String name, String email, String password){
         super(name, email, password);
+
+    }
+    @Override
+    public void showProfile(){
+        System.out.println("Student Profile");
+        System.out.println("ID: "+ getId() +"\n"+
+                    "Name: "+ getName()+"\n"+
+                    "Email: "+ getEmail());
+        return;
+
+    }
+    @Override
+    public void showDashboard(){
+        Scanner sc = new Scanner(System.in);
+       while (true){
+           System.out.println("Student Dashboard");
+           System.out.println("1. Show Profile\n2. Logout");
+           int dashboardChoice = sc.nextInt();
+           switch(dashboardChoice){
+               case 1:
+                   showProfile(); break;
+               case 2: return;
+           }
+       }
+
     }
 }
