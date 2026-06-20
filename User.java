1d52ac523b12d06386629cfdc3cf691d4d29e846
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-06-21 04:36:09 +0530

    Improve User class methods and add dashboard/profile
    Refactor User class methods for better readability and error handling.
    improved name, email, password, and validation.
    added dashboard and profile method

--- User.java
+++ User.java
@@ -7,12 +7,13 @@ public class User {
     private String email;
     private String password;
 
-    public User(String name, String email, String password){
+    public User(String name, String email, String password) {
         this.id = nextId++;
         setName(name);
         updateEmail(email);
         updatePassword(password);
     }
+
     public int getId() {
         return id;
     }
@@ -23,30 +24,48 @@ public class User {
     }
 
     public void setName(String name) {
-        if(name == null){
+        if (name == null) {
             System.out.println("Empty name is not allowed.");
+            return;
         }
         this.name = name;
     }
+
     // email update and get
-    public String getEmail() { return email; }
+    public String getEmail() {
+        return email;
+    }
 
     public void updateEmail(String email) {
-        if (email == null){ System.out.println("Enter your email"); }
-        if (email.matches("^[A-Za-z0-9_.-]+@[A-Za-z0-9_.-]+$")){
-        this.email = email;  }}
+        if (email == null) {
+            System.out.println("Enter your email");
+            return;
+        }
+        if (email.matches("^[A-Za-z0-9_.-]+@[A-Za-z0-9_.-]+$")) {
+            this.email = email;
+        }
+    }
 
-    public String getPassword() { return password; }
+    public String getPassword() {
+        return password;
+    }
 
-    public void updatePassword(String password) { this.password = password; }
+    public void updatePassword(String password) {
+        if (password == null) {
+            System.out.println("Enter your Password");
+            return;
+        }
+        this.password = password;
+    }
 
-//    public class login(){
-//
-//    }
-//    public class logout(){
-//
-//    }
-//    public class showprofile(){
-//
-//    }
+    public void showDashboard() {
+        System.out.println("User Dashboard");
+    }
+
+    public void showProfile() {
+        System.out.println("User Profile");
+    }
 }
+
+
+
