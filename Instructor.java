f66b33bf2dcf8ad1e661ba16147074a6b9c704e8
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-06-21 04:43:07 +0530

    Refactor Instructor class methods and constructor
    used the profile and dashboard method from the User class.
    used polymorphism override type to perform accordingly

--- Instructor.java
+++ Instructor.java
@@ -1,6 +1,32 @@
+import java.lang.Override;
+import java.util.Scanner;
 public class Instructor extends User{
-public  Instructor(String instructorName, String instructorEmail, String instructorPassword)
+public  Instructor(String name, String email, String password)
     {
         super(name, email, password);
     }
-}
+    @Override
+    public void showProfile(){
+        System.out.println("Instructor Profile");
+        System.out.println("ID: "+ getId() +"\n"+
+                "Name: "+ getName()+"\n"+
+                "Email: "+ getEmail());
+
+    }
+
+    @Override
+    public void showDashboard(){
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println("Instructor Dashboard");
+            System.out.println("1. Show Profile\n2. Logout");
+            int dashboardChoice = sc.nextInt();
+            switch(dashboardChoice){
+                case 1:
+                    showProfile(); break;
+                case 2: return;
+            }
+        }
+                }
+    }
+
