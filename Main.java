eefde4d9f3e5df32ca513e71af57838672ee5609
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-06-21 04:48:14 +0530

    Refactor login, registration flow  and storing user in Main.java
    Learned the ArrayList collection framework to store users.
    created register method for users
    created login method for users 
    created dashboard and show profile methods after logging in and improved logged navigation

--- Main.java
+++ Main.java
@@ -1,61 +1,94 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main {
+    static  ArrayList<User> users = new ArrayList<>();
     public static void main(String[] args) {
+
         // #Home
         System.out.println("\n======================");
         System.out.println("LEARNING MANAGEMENT SYSTEM");
         System.out.println("======================");
-        System.out.println("1. Login\n2. Register\n3. Exit");
 
-        Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("Select Login Type");
-                System.out.println("\n1. Student\n2. Instructor\n3. Admin");
-                int loginChoice = sc.nextInt();
+        while (true) {
+            System.out.println("1. Login\n2. Register\n3. Exit");
 
-                break;
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1: //#login
+                    User loggedInUser = loginUser();
+                    if(loggedInUser != null){
+                        loggedInUser.showDashboard();
+                    } else {
+                        System.out.println("Invalid Id or Password !!!");
+                    }
+                    break;
 
-            case 2:
-                System.out.println("Register Type");
-                System.out.println("\n1. Student\n2. Instructor\n");
-                int registerChoice = sc.nextInt();
+                case 2:
+                    System.out.println("Register Type");
+                    System.out.println("\n1. Student\n2. Instructor\n");
+                    int registerChoice = sc.nextInt();
 
-                switch (registerChoice) {
-                    case 1:
-                        registerStudent();
-                        break;
-                    case 2:
-                        registerinstructor();
-                        break;
-                    default:
-                        System.out.println("Enter valid input");
+                    switch (registerChoice) {
+                        case 1:
+                            registerStudent();
+                            break;
+                        case 2:
+                            registerinstructor();
+                            break;
+                        default:
+                            System.out.println("Enter valid input");
+                            return;
+                    } break;
+                case 3:
+                    System.out.println("Thanks for Using SKS AI LMS");
+                    return;
+                default:
+                    System.out.println("Please enter values from options");
+            }
 
-                }
         }
-
-
     }
-
     public static void registerStudent() {
         System.out.println("Enter Name:\nEnter Email\nEnter Password");
         Scanner sc = new Scanner(System.in);
-        String studentName = sc.next();
+        String studentName = sc.nextLine();
         String studentEmail = sc.next();
         String studentPassword = sc.next();
         Student student = new Student(studentName, studentEmail, studentPassword);
-        System.out.println(" You have registered Successfully");
+        users.add(student);
+
+        System.out.println("You have registered Successfully");
         System.out.println("Your Id: " + student.getId());
     }
     public static void registerinstructor(){
         System.out.println("Enter Name:\nEnter Email\nEnter Password");
         Scanner sc = new Scanner(System.in);
-        String instructorName = sc.next();
+        String instructorName = sc.nextLine();
         String instructorEmail = sc.next();
         String instructorPassword = sc.next();
         Instructor instructor = new Instructor(instructorName, instructorEmail, instructorPassword);
+        users.add(instructor);
         System.out.println(" You have registered Successfully");
         System.out.println("Your Id: " + instructor.getId());
     }
+    public static User loginUser(){
+        System.out.println("Enter details to Login !!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Id: \nPassword: ");
+        int inputId = sc.nextInt();
+        sc.skip("(\r\n|[\n\r\u2028\u2029\u0085]?)");
+        String inputPassword = sc.next();
+        for(User user: users){
+            if (inputId == user.getId() && inputPassword.equals(user.getPassword())){
+                System.out.println("Welcome "+ user.getName());
+                return user;
+            }
+        }
+        return null;
+    }
+
+
+
 }
