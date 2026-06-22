cefe2b518179bd53244f3cacf880841b6a4294d3
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-06-22 17:13:41 +0530

    Rename Instructor class to Student and update methods

--- Instructor.java
+++ Instructor.java
@@ -1,25 +1,31 @@
 import java.lang.Override;
 import java.util.Scanner;
 
-public class Instructor extends User {
-    public Instructor(String name, String email, String password) {
+public class Student extends User {
+
+    public Student(String name, String email, String password) {
         super(name, email, password);
+
     }
+
+    Scanner sc = new Scanner(System.in);
 
     @Override
     public void showProfile() {
-        System.out.println("Instructor Profile");
+        System.out.println("Student Profile");
         System.out.println("ID: " + getId() + "\n" +
                 "Name: " + getName() + "\n" +
                 "Email: " + getEmail());
         updatedMenu();
+
+
     }
 
     @Override
     public void showDashboard() {
-        Scanner sc = new Scanner(System.in);
+
         while (true) {
-            System.out.println("Instructor Dashboard");
+            System.out.println("Student Dashboard");
             System.out.println("1. Show Profile\n2. Logout");
             int dashboardChoice = sc.nextInt();
             switch (dashboardChoice) {
@@ -30,6 +36,7 @@ public class Instructor extends User {
                     return;
             }
         }
+
     }
 }
 
