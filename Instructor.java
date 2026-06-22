3c2db02c7f1f2a7498417649d41e50936d7045c8
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-06-22 17:13:17 +0530

    Refactor Instructor class methods for consistency

--- Instructor.java
+++ Instructor.java
@@ -1,32 +1,35 @@
 import java.lang.Override;
 import java.util.Scanner;
-public class Instructor extends User{
-public  Instructor(String name, String email, String password)
-    {
+
+public class Instructor extends User {
+    public Instructor(String name, String email, String password) {
         super(name, email, password);
     }
-    @Override
-    public void showProfile(){
-        System.out.println("Instructor Profile");
-        System.out.println("ID: "+ getId() +"\n"+
-                "Name: "+ getName()+"\n"+
-                "Email: "+ getEmail());
 
+    @Override
+    public void showProfile() {
+        System.out.println("Instructor Profile");
+        System.out.println("ID: " + getId() + "\n" +
+                "Name: " + getName() + "\n" +
+                "Email: " + getEmail());
+        updatedMenu();
     }
 
     @Override
-    public void showDashboard(){
+    public void showDashboard() {
         Scanner sc = new Scanner(System.in);
-        while (true){
+        while (true) {
             System.out.println("Instructor Dashboard");
             System.out.println("1. Show Profile\n2. Logout");
             int dashboardChoice = sc.nextInt();
-            switch(dashboardChoice){
+            switch (dashboardChoice) {
                 case 1:
-                    showProfile(); break;
-                case 2: return;
+                    showProfile();
+                    break;
+                case 2:
+                    return;
             }
         }
-                }
     }
+}
 
