20344aefb732afaa55e0c8bc1b26a7920b274f27
Sumit Kumar Sharma <sumitkumarsharma008@gmail.com>
2026-06-28 05:13:53 +0530

    Enhance Student class with course management
    Refactored Student class to include course enrollment features and updated dashboard options.

--- Student.java
+++ Student.java
@@ -1,34 +1,96 @@
 import java.lang.Override;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Student extends User {
 
-    public Student(String name, String email, String password){
+    public Student(String name, String email, String password) {
         super(name, email, password);
 
     }
+
+    private ArrayList<Course> enrolledCourse = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+
     @Override
-    public void showProfile(){
+    public void showProfile() {
         System.out.println("Student Profile");
-        System.out.println("ID: "+ getId() +"\n"+
-                    "Name: "+ getName()+"\n"+
-                    "Email: "+ getEmail());
-        return;
+        System.out.println("ID: " + getId() + "\n" +
+                "Name: " + getName() + "\n" +
+                "Email: " + getEmail());
+        updatedMenu();
+
 
     }
-    @Override
-    public void showDashboard(){
-        Scanner sc = new Scanner(System.in);
-       while (true){
-           System.out.println("Student Dashboard");
-           System.out.println("1. Show Profile\n2. Logout");
-           int dashboardChoice = sc.nextInt();
-           switch(dashboardChoice){
-               case 1:
-                   showProfile(); break;
-               case 2: return;
-           }
-       }
 
+
+    @Override
+    public void showDashboard() {
+
+        while (true) {
+            System.out.println("Student Dashboard");
+            System.out.println("1. Show Profile\n2. View Available courses\n3. My courses\n4. Logout");
+            int dashboardChoice = sc.nextInt();
+            sc.nextLine();
+            switch (dashboardChoice) {
+                case 1:
+                    showProfile();
+                    break;
+                case 2:
+                    viewAvailableCourse(); enrollCourse();
+                    break;
+                case 3:
+                    myCourse();
+                    break;
+                case 4:
+                    return;
+            }
+        }
+
+    }
+
+    public void viewAvailableCourse() {
+
+        for (Course course : Main.courses) {
+            System.out.println("\n" + course.getCourseName() + ": " + course.getCourseId());
+        }
+    }
+
+    public void enrollCourse() {
+        System.out.print("Enter Course ID: ");
+        int inputCourseID = sc.nextInt();
+        Course selectedCourse = null;
+        boolean courseExistence = false;
+        //first loops to find course
+        for (Course course : Main.courses) {
+            if (course.getCourseId() == inputCourseID) {
+                selectedCourse = course;
+                break;
+            }
+        }
+        if (selectedCourse != null) {
+            for (Course course1 : enrolledCourse) {
+                if (course1.getCourseId() == selectedCourse.getCourseId()) {
+                    courseExistence = true;
+                    break;
+                }
+            }
+            if (!courseExistence) {
+                enrolledCourse.add(selectedCourse);
+                System.out.println("Enrolled Successfully into " + selectedCourse.getCourseName());
+            } else {
+                System.out.println(" Already Enrolled");
+            }
+        } else {
+            System.out.println("Course not found");
+        }
+    }
+
+    public void myCourse() {
+
+        for (Course course : enrolledCourse) {
+            System.out.println("\n" + course.getCourseName() + ": " + course.getCourseId());
+        }
     }
 }
+
